4da41c256c565082028281ab0b83c0bf4449d47a
Alexander Efremov <a.efremov@l-tech.ru>
2018-11-28 18:58:15 +0300

    Project refactoring.

--- app/src/main/java/com/aefremov/cstmrtngbr/MainActivity.kt
+++ app/src/main/java/com/aefremov/cstmrtngbr/MainActivity.kt
@@ -2,11 +2,22 @@ package com.aefremov.cstmrtngbr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.aefremov.rtngbr.SimpleRatingBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rating_bar.setOnRatingChangeListener(object : SimpleRatingBar.RatingChangeListener {
+            override fun onRatingChange(rating: Float) {
+                Toast.makeText(this@MainActivity, rating.toString(), Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        rating_bar.setRating(3f)
     }
 }
